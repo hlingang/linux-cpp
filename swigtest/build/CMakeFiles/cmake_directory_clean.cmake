428433de18b794f7778275a4d6c15d688cc31c52
huanglingang <lingang_huang@163.com>
2022-11-13 08:01:39 +0000

    [mod] delete cache file

--- swigtest/build/CMakeFiles/cmake_directory_clean.cmake
+++
@@ -1,5 +0,0 @@
-file(REMOVE_RECURSE
-  "../python/cxx/testlibPYTHON_wrap.cxx"
-  "../python/ctestlib.py"
-  "CMakeFiles/ctestlib.dir/testlibPYTHON.stamp"
-)
--- swigtest/build/CMakeFiles/ctestlib.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ctestlib.
-# This may be replaced when dependencies are built.
