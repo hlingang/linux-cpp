6b9261282af98fb404f28e41781a2e867d577f21
huanglingang <lingang.huang@vastaitech.com>
2023-11-01 13:58:33 +0800

    [mod] rename struct

--- loadsohelper/liba.h
+++ loadsohelper/liba.h
@@ -2,12 +2,12 @@
 #define __LIBA_H_
 #include "load_so_helper.h"
 
-typedef int ( *T_test01 )( int );
-typedef void ( *T_test02 )( const char* );
+typedef int ( *test01_t )( int );
+typedef void ( *test02_t )( const char* );
 struct S_LIBASO
 {
-    FuncSymbol< T_test01 > test01{ NULL, "test01" };  ///< function address
-    FuncSymbol< T_test02 > test02{ NULL, "test02" };  ///< function address
+    FuncSymbol< test01_t > test01{ NULL, "test01" };  ///< function address
+    FuncSymbol< test02_t > test02{ NULL, "test02" };  ///< function address
     void*                  handler{ NULL };           ///< so handler
     void*                  so = ( void* )"liba.so";   ///< so name
 };
