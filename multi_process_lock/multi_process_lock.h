e07704a2a32f62379189c95d8c8e481cc1315993
huanglingang <lingang.huang@vastaitech.com>
2023-11-01 16:58:54 +0800

    [mod] add process lock test

---
+++ multi_process_lock/multi_process_lock.h
@@ -0,0 +1,43 @@
+#include <unistd.h>
+#include <semaphore.h>
+#include <string>
+#include <fcntl.h>
+#include <stdexcept>
+
+#define DEFAULT_NAME "multi_process_sem_lock"
+
+using namespace std;
+
+class MultiLock
+{
+    public:  
+    MultiLock(): MultiLock(DEFAULT_NAME)
+    {
+    }
+    MultiLock(const char* name):m_psem(nullptr), m_name(name)
+    {
+        m_psem = sem_open(m_name.c_str(), O_RDWR|O_CREAT, 0664, 1 );
+        if(m_psem == SEM_FAILED)
+        {
+            perror("sem_open fail");
+            m_psem = nullptr;
+            throw runtime_error("sem open fail");
+        }
+        if( m_psem )
+        {
+            sem_wait(m_psem );
+        }
+    }
+    ~MultiLock()
+    {
+        if( m_psem )
+        {
+            sem_post( m_psem );
+            sem_close( m_psem );
+            sem_unlink( m_name.c_str() );
+        }
+    }
+    private:  
+    sem_t* m_psem;
+    string m_name;
+};
---
+++ multi_process_lock/test.cpp
@@ -0,0 +1,13 @@
+#include "multi_process_lock.h"
+
+int main()
+{
+    if( 1 )
+    {
+        MultiLock lock;
+        printf("lock pid=%d\n", (int)getpid());
+        sleep(10);
+    }
+    printf("unlock\n");
+    sleep(5);
+}
