f44826edc2a025a793701155ff7b061c7cd60b4b
huanglingang <lingang_huang@vastaitech.com>
2025-05-21 14:37:04 +0800

    [mod] struct name

--- dynamic_param_save/test.cpp
+++ dynamic_param_save/test.cpp
@@ -11,7 +11,6 @@
 using namespace std;
 
 // 如何接受动态参数并且进行保存 和 传递
-
 template < size_t... Is > struct __S_index_seq
 {
 };
@@ -23,18 +22,19 @@ template < size_t... N > struct __S_index_helper< 0, 0, N... >
     using type = __S_index_seq< 0, N... >;
 };
 
-template < typename... Args > struct __S_fmt_aux
+template < typename... Args > struct __S_params_saver
 {
     using index_helper_t = typename __S_index_helper< sizeof...( Args ) >::type;
     tuple< Args... > m_tu;
-    __S_fmt_aux( Args&&... args ) : m_tu( std::forward< Args >( args )... ) {}
+    // 保存 动态参数 //
+    __S_params_saver( Args&&... args ) : m_tu( std::forward< Args >( args )... ) {}
     int operator()( const char* fmt )  // 类型萃取 //
     {
-        index_helper_t __instance;
-        __display_params( fmt, __instance );
+        __display_params( fmt, typename __S_index_helper< sizeof...( Args ) >::type() );
         return 0;
     }
-    template < size_t... Is > void __display_params( const char* fmt, __S_index_seq< Is... >& t )
+    // 此处通过显示的参数传递 实现 对类型 的萃取 //
+    template < size_t... Is > void __display_params( const char* fmt, const __S_index_seq< Is... >& )
     {
         printf( fmt, get< Is >( m_tu )... );
     }
@@ -46,14 +46,13 @@ template < typename... Args > struct __S_fmt_aux
         return typename __S_index_helper< sizeof...( Args ) >::type();
     }
 };
-template < typename... Args > __S_fmt_aux< Args... > fmt_aux( Args... args )
+template < typename... Args > __S_params_saver< Args... > params_saver( Args... args )
 {
-    return __S_fmt_aux< Args... >( std::forward< Args >( args )... );
+    return __S_params_saver< Args... >( std::forward< Args >( args )... );
 }
 
 int main()
-
 {
-    auto s = fmt_aux( 1, "ab", 'c', 3.14 );
-    s( "display parameters: $1=%d, $2=%s, $3=%c, $4=%.2f\n" );
+    auto s = params_saver( 1, "ab", 'c', 3.14 );
+    s( "display saved parameters: $1=%d, $2=%s, $3=%c, $4=%.2f\n" );
 }
