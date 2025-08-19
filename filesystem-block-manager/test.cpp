c98b5cdd91125208c79cc9ad02956f65aee77a9c
huanglingang <lingang_huang@vastaitech.com>
2025-08-19 18:32:36 +0800

    [mod] update file system

--- filesystem-block-manager/test.cpp
+++ filesystem-block-manager/test.cpp
@@ -19,6 +19,7 @@ using namespace std;
 #define NR_BIT_OFFSET( nr ) ( nr % NR_BITS_PER_LONG )
 
 #define PAGE_SIZE ( 4096 )
+#define PAGE_MASK ( PAGE_SIZE - 1 )
 #define BLOCK_SIZE ( 1024 )
 #define NR_MAX_BLOCKS ( 1024 )
 #define NR_BLOCKS_PER_GROUP ( 256 )
@@ -96,6 +97,7 @@ struct buffer_head
     void*          data;
     unsigned long  offset;
     int            uptodate;
+    int            mapped;
 };
 
 #define buf_ctl( type, x ) ( ( type )( x ) )
@@ -170,24 +172,33 @@ void free_one_page( struct page_t* page )
     page_bitmap[ index ] = 0;
 }
 
+static inline void map_bh( struct buffer_head* bh, unsigned long block )
+{
+    if ( !bh )
+        return;
+    bh->nr_block = block;
+    bh->mapped   = 1;
+}
+
 struct buffer_head* create_buffer_head( space_mapping* mapping, struct page_t* page, unsigned long index,
                                         unsigned iblock )
 {
-    unsigned long offset = PAGE_SIZE;
-    buffer_head*  head   = NULL;
+    unsigned long       offset = PAGE_SIZE;
+    struct buffer_head* head   = NULL;
     do
     {
+        offset -= BLOCK_SIZE;
         struct buffer_head* bh                                       = new struct buffer_head;
         bh->next                                                     = NULL;
         bh->page                                                     = page;
         bh->offset                                                   = offset;
         bh->next                                                     = head;
-        bh->nr_block                                                 = iblock;
+        bh->nr_block                                                 = -1;
+        bh->mapped                                                   = 0;
         bh->data                                                     = ( char* )page->vir + offset;
         head                                                         = bh;
         mapping->buffer_head_mapping[ index ][ offset / BLOCK_SIZE ] = bh;
-        ++iblock;
-    } while ( ( offset -= BLOCK_SIZE ) > 0 );
+    } while ( offset > 0 );
     page->bh = head;
     return page->bh;
 }
@@ -207,9 +218,10 @@ struct buffer_head* read_block( struct buffer_head* bh )
 }
 struct buffer_head* write_block( struct buffer_head* bh )
 {
-    unsigned      nr_block = bh->nr_block;
+    unsigned long nr_block = bh->nr_block;
     block_data_t* ptr      = g_blocks + nr_block;
     memcpy( ptr, bh->data, BLOCK_SIZE );
+    printf( "copy data from (%p) to block(%p, %lu): %s\n", bh->data, ptr, nr_block, ( char* )ptr );
     bh->uptodate = 1;
     return bh;
 }
@@ -230,17 +242,7 @@ struct buffer_head* get_blk_bh( struct inode_t* inode, unsigned long iblock )
     {
         create_buffer_head( ( struct space_mapping* )&inode->i_mapping, page, index, block );
     }
-    struct buffer_head* bh = page->bh;
-    while ( bh != NULL )
-    {
-        if ( bh->nr_block == iblock )
-        {
-            break;
-        }
-        bh = bh->next;
-    }
-    read_block( bh );
-    return bh;
+    return page->bh;
 }
 struct buffer_head* sb_bread( struct super_block_t* sb, unsigned long iblock )
 {
@@ -255,12 +257,16 @@ struct buffer_head* sb_bread( struct super_block_t* sb, unsigned long iblock )
     struct buffer_head* bh = page->bh;
     while ( bh != NULL )
     {
-        if ( bh->nr_block == iblock )
+        if ( block == iblock )
         {
+            map_bh( bh, iblock );
             break;
         }
+        block++;
         bh = bh->next;
     }
+    if ( !bh )
+        return NULL;
     read_block( bh );
     return bh;
 }
@@ -348,6 +354,7 @@ out:
 }
 int __get_block( super_block_t* sb, unsigned long ngp )
 {
+    cout << "get_block" << endl;
     if ( ngp >= sb->nr_groups )
         ngp = 0;
     unsigned long ino;
@@ -381,10 +388,11 @@ unsigned long modify_inode_size( struct inode_t* inode, ssize_t sz )
     inode->size += sz;
     return inode->size;
 }
-struct buffer_head* get_blocks( struct inode_t* inode, unsigned long iblock )
+unsigned long get_blocks( struct buffer_head* bh, struct inode_t* inode, unsigned long iblock )
 {
     unsigned long* bmd;
     unsigned long* bte;
+    cout << "get_blocks iblock = " << iblock << endl;
     if ( iblock < DIRECT_BLOCK )
     {
         bte = &inode->i_data[ iblock ];
@@ -398,26 +406,79 @@ struct buffer_head* get_blocks( struct inode_t* inode, unsigned long iblock )
             inode->i_data[ DIRECT_BLOCK ] = get_block( inode->sb );
         bmd = ( unsigned long* )sb_bread( inode->sb, inode->i_data[ DIRECT_BLOCK ] )->data + bmd_offset;
         if ( !*bmd )
+        {
             *bmd = get_block( inode->sb );
-        bte = ( unsigned long* )sb_bread( inode->sb, *bmd )->data + bte_offset;
+        }
+        printf( "bmd_block:%lu, bte_offset = %lu, addr = %p\n", *bmd, bte_offset, sb_bread( inode->sb, *bmd )->data );
+        bte = ( unsigned long* )( sb_bread( inode->sb, *bmd )->data ) + bte_offset;
     }
-    if ( *bte )
+    if ( !*bte )
+    {
         *bte = get_block( inode->sb );
-    struct buffer_head* bh = get_blk_bh( inode, iblock );
-    return bh;
+    }
+    cout << "*bte = " << *bte << endl;
+    unsigned long target = *bte;
+    map_bh( bh, target );
+    return target;
 }
 int write_inode( struct inode_t* inode, const char* buf, unsigned long len )
 {
-    struct buffer_head* bh;
-    unsigned            pos    = inode->size;
-    unsigned            iblock = pos >> BLOCK_SHIFT;
-    bh                         = get_blocks( inode, iblock );
-    char* addr                 = ( char* )bh->data;
+    struct buffer_head *head, *bh;
+    unsigned long       pos = inode->size;
+
+    unsigned long iblock = pos >> BLOCK_SHIFT;
+    printf( "inode size = %lu,  write inode iblock=%lu\n", pos, iblock );
+    head = get_blk_bh( inode, iblock );
+    bh   = head;
+    while ( bh )
+    {
+        if ( !bh->mapped )
+            get_blocks( bh, inode, iblock );
+        iblock++;
+        printf( "bh->pblock(%lu)\n", bh->nr_block );
+        bh = bh->next;
+    }
+    unsigned long start = pos & PAGE_MASK;
+    unsigned long end   = start + len;
+    if ( end >= PAGE_SIZE )
+        end = PAGE_SIZE;
+    len           = ( end - start ) & PAGE_MASK;
+    void*    addr = ( char* )head->page->vir + start;
+    unsigned block_start, block_end;
+    for ( block_start = 0, block_end = 0, bh = head; block_start < PAGE_SIZE; )
+    {
+        block_end = block_start + BLOCK_SIZE;
+        printf( "pblock(%lu), block_start:%lu-block_end:%lu\n", bh->nr_block, block_start, block_end );
+        if ( block_start < end && block_end > start )
+        {
+            if ( block_end > end || block_start < start )
+            {
+                read_block( bh );
+            }
+        }
+        block_start = block_end;
+        bh          = bh->next;
+    }
     memcpy( addr, buf, len );
-    write_block( bh );
-    modify_inode_size( inode, BLOCK_SIZE );
-    block_data_t* block_data = g_blocks + bh->nr_block;
-    printf( "blk:%lu data: %s\n", bh->nr_block, ( const char* )block_data );
+    printf( "dump page(addr=%p): %s\n", addr, ( char* )addr );
+    modify_inode_size( inode, len );
+    unsigned long blk = 0;
+    for ( block_start = 0, block_end = 0, bh = head; block_start < PAGE_SIZE; )
+    {
+        block_end = block_start + BLOCK_SIZE;
+        printf( "block_start:%lu, block_end:%lu, start:%lu, end:%lu\n", block_start, block_end, start, end );
+        if ( block_start < end && block_end > start )
+        {
+            write_block( bh );
+            printf( "write block: bh->nr_block(%lu)\n", bh->nr_block );
+            blk = bh->nr_block;
+        }
+        block_start = block_end;
+        bh          = bh->next;
+    }
+    block_data_t* block_data = g_blocks + blk;
+    printf( "blk:%lu data: %s\n", blk, ( const char* )block_data );
+    printf( "============================================================\n" );
     return len;
 }
 //===============================================================
@@ -433,8 +494,8 @@ int main()
     memset( g_blocks, 0x00, sizeof( struct block_data_t ) * 1024 );
     ptr = g_blocks;
     init_global_block( ptr, 1024 );
-    ptr = g_blocks;
-    stats_global_block( ptr, 1024 );
+    // ptr = g_blocks;
+    // stats_global_block( ptr, 1024 );
 
     // ============== 创建 page ==============
     g_page_map = new struct page_t[ 1024 ];
