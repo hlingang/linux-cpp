d50428d620701b25a41b7337f355e58611dfb09f
huanglingang <lingang_huang@vastaitech.com>
2025-12-09 16:06:04 +0800

    [mod] add invoker-impl

---
+++ invoker-impl/invoker.h
@@ -0,0 +1,91 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <memory>
+#include <mutex>
+#include <condition_variable>
+#include <sstream>
+#include <string>
+#include <unistd.h>
+#include <tuple>
+#include <utility>
+
+using namespace std;
+
+struct BaseInvoker
+{
+    virtual void operator()() {};
+};
+// 萃取 tuple size
+template < typename... Args > struct _S_tuple_size
+{
+    static const size_t value = sizeof...( Args );
+};
+// 模板特例进行类型萃取 //
+template < typename... Args > struct _S_tuple_size< tuple< Args... > >
+{
+    static const size_t value = sizeof...( Args );
+};
+// index 序列保存 //
+template < size_t... Is > struct _S_tuple_index
+{
+};
+// 递归获取序列 //
+template < size_t... Is > struct _S_tuple_seq
+{
+    using type = __undefined;
+};
+template < size_t N, size_t... M > struct _S_tuple_seq< N, M... > : _S_tuple_seq< N - 1, N - 1, M... >
+{
+};
+template < size_t... Is > struct _S_tuple_seq< 0, 0, Is... >
+{
+    using type = _S_tuple_index< 0, Is... >;
+};
+template < size_t N > typename _S_tuple_seq< N >::type _S_make_tuple_index()
+{
+    return typename _S_tuple_seq< N >::type();
+}
+// 将 tuple 打包成 可调用对象
+template < typename _Tuple > struct Invoker : public BaseInvoker
+{
+    _Tuple _M_tuple;
+    Invoker( _Tuple&& _t ) : _M_tuple( std::move( _t ) ) {}
+    template < size_t... Is > void _M_invoke( _S_tuple_index< Is... > )
+    {
+        return std::__invoke( get< Is >( _M_tuple )... );
+    }
+    void operator()()
+    {
+        return _M_invoke( _S_make_tuple_index< _S_tuple_size< decltype( _M_tuple ) >::value >() );
+    }
+};
+class InvokerWrap
+{
+public:
+    template < typename Callable, typename... Args > InvokerWrap( Callable&& _f, Args... args )
+    {
+        using _Tuple = tuple< Callable, Args... >;
+        _S_invoker =
+            _S_make_invoker( _S_make_tuple( std::forward< Callable >( _f ), std::forward< Args >( args )... ) );
+        _S_invoker->operator()();
+    }
+    template < typename Callable, typename... Args >
+    tuple< Callable, Args... > _S_make_tuple( Callable&& _f, Args... args )
+    {
+        using _Tuple = tuple< Callable, Args... >;
+        return _Tuple{ std::forward< Callable >( _f ), std::forward< Args >( args )... };
+    }
+    template < typename _Tuple > Invoker< _Tuple >* _S_make_invoker( _Tuple&& _t )
+    {
+        return new Invoker< _Tuple >( std::forward< _Tuple >( _t ) );
+    }
+    ~InvokerWrap()
+    {
+        delete _S_invoker;
+    }
+
+private:
+    // 基类指针保存 [具体实现对象指针]
+    BaseInvoker* _S_invoker;
+};
---
+++ invoker-impl/test.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <algorithm>
+#include <memory>
+#include <tuple>
+#include <sstream>
+#include <thread>
+#include "invoker.h"
+
+using namespace std;
+
+int main()
+{
+
+    auto f         = []( int a, const std::string& str ) -> void { printf( "Invoke test[%d]: %s\n", a, str.c_str() ); };
+    char buf[ 32 ] = { 0 };
+    for ( int i = 0; i < 10; i++ )
+    {
+        snprintf( buf, sizeof( buf ), "I am Invoker-%d", i );
+        InvokerWrap( f, i + 10, buf );
+    }
+    return 0;
+}
