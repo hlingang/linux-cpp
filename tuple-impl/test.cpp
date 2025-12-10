78c34d63f81e2446f491cda6faf3fadf4c826408
huanglingang <lingang_huang@vastaitech.com>
2025-12-10 12:58:15 +0800

    [mod] update tuple-impl

--- tuple-impl/test.cpp
+++ tuple-impl/test.cpp
@@ -4,6 +4,27 @@
 
 using namespace std;
 
+/**
+
+类型转换关系图示:
+子类和任意父类间可以发生自动隐式转换
+_Tuple_impl<0, int, double, std::string, char>           // get<0>(t) 函数传参发生自动隐式转化
+    |                                                        |
+    +-- _Tuple_impl<1, double, std::string, char>        // get<1>(t) 函数传参发生自动隐式转化
+    |       |                                                |
+    |       +-- _Tuple_impl<2, std::string, char>        // get<2>(t) 函数传参发生自动隐式转化
+    |       |       |                                        |
+    |       |        +-- _Tuple_impl<3, char>            // get<3>(t) 函数传参发生自动隐式转化
+    |       |                |                               |
+    |       |                +-- _Tuple_base<3, char>    // _Tuple_base::get()
+    |       |
+    |       |        +-- _Tuple_base<2, std::string>
+    |       |
+    |       +-- _Tuple_base<1, double>
+    |
+    +-- _Tuple_base<0, int>
+ */
+
 int main()
 {
     ns_tuple::tuple< int, float, string, double, char > t( 99, 3.14, "abc", 100.01, 'A' );
