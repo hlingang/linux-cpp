0117320b4e012fea488c526df8bbe2e0fc668e84
huanglingang <lingang_huang@vastaitech.com>
2025-12-10 12:36:04 +0800

    [mod] update

--- tuple-impl/tuple-impl.h
+++ tuple-impl/tuple-impl.h
@@ -15,9 +15,10 @@ namespace ns_tuple
 {
 template < size_t N, typename Tp > struct _Tuple_base
 {
-    Tp value;
+    Tp value;  // 存储具体类型的值
+    // 具体类型存储值的初始化
     _Tuple_base( Tp&& __value ) : value( std::forward< Tp >( __value ) ) {}
-    // 此处定义 get() 函数的目的是为了完成入参类型向基类类型的自动类型转换 //
+    // 获取具体类型存储的值，同时完成入参向基类的自动类型转换
     static Tp get( const _Tuple_base& _tb )
     {
         return _tb.value;
@@ -30,15 +31,15 @@ template < size_t N, typename... Args > struct _Tuple_impl
 // 特例模板
 template < size_t N, typename Tp > struct _Tuple_impl< N, Tp > : _Tuple_base< N, Tp >
 {
-    using _Base = _Tuple_base< N, Tp >;
+    using _Base = _Tuple_base< N, Tp >;  // 声明 _Base 类型
     _Tuple_impl( Tp&& _t ) : _Base( std::forward< Tp >( _t ) ) {}
 };
 // 实现递推
 template < size_t N, typename Tp, typename... Args >
 struct _Tuple_impl< N, Tp, Args... > : _Tuple_base< N, Tp >, _Tuple_impl< N + 1, Args... >
 {
-    using _Inherited = _Tuple_impl< N + 1, Args... >;
-    using _Base      = _Tuple_base< N, Tp >;
+    using _Base      = _Tuple_base< N, Tp >;           // 声明 _Base 类型
+    using _Inherited = _Tuple_impl< N + 1, Args... >;  // 声明 _Inherited 类型
     _Tuple_impl( Tp&& _t, Args&&... args )
         : _Base( std::forward< Tp >( _t ) ), _Inherited( std::forward< Args >( args )... )
     {
