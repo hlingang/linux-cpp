602f1ac03e7c60e98d1bf89da6881db216a7abef
huanglingang <lingang_huang@vastaitech.com>
2026-01-30 18:09:15 +0800

    [mod] add member-function-invoker

---
+++ member-function-invoker/invoker.h
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <memory>
+#include <mutex>
+#include <condition_variable>
+#include <sstream>
+#include <string>
+#include <unistd.h>
+#include <tuple>
+#include <utility>
+
+using namespace std;
+
+class MemberCls
+{
+public:
+    struct MemberOps_t
+    {
+        int ( MemberCls::*__call )( int a );
+        MemberOps_t() : __call( nullptr ) {}
+    };
+    int memberFunction( int a )
+    {
+        printf( "Member Function Invoke test[%d]\n", a );
+        return 0;
+    }
+    MemberOps_t _M_ops;
+    MemberCls()
+    {
+        _M_ops.__call = &MemberCls::memberFunction;
+    }
+};
---
+++ member-function-invoker/test.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <algorithm>
+#include <memory>
+#include <tuple>
+#include <sstream>
+#include <thread>
+#include "invoker.h"
+
+using namespace std;
+
+int ( MemberCls::*member_invoker )( int );
+
+int main()
+{
+
+    MemberCls  obj;
+    MemberCls* pObj = &obj;
+    member_invoker  = &MemberCls::memberFunction;
+    for ( int i = 0; i < 30; i += 3 )
+    {
+        ( obj.*member_invoker )( i );
+        ( obj.*obj._M_ops.__call )( i + 1 );
+        ( pObj->*obj._M_ops.__call )( i + 2 );
+    }
+}
