2b13f31b2a20963501e40f1a74b995746b1bdffc
huanglingang <lingang_huang@vastaitech.com>
2026-01-30 18:16:15 +0800

    [mod] update member-function invoker

--- member-function-invoker/test.cpp
+++ member-function-invoker/test.cpp
@@ -19,10 +19,15 @@ int main()
     MemberCls  obj;
     MemberCls* pObj = &obj;
     member_invoker  = &MemberCls::memberFunction;
-    for ( int i = 0; i < 30; i += 3 )
+    for ( int i = 0; i < 30; i += 6 )
     {
         ( obj.*member_invoker )( i );
-        ( obj.*obj._M_ops.__call )( i + 1 );
-        ( pObj->*obj._M_ops.__call )( i + 2 );
+        ( pObj->*member_invoker )( i + 1 );
+
+        ( obj.*( obj._M_ops.__call ) )( i + 2 );
+        ( pObj->*( obj._M_ops.__call ) )( i + 3 );
+
+        ( obj.*( pObj->_M_ops.__call ) )( i + 4 );
+        ( pObj->*( pObj->_M_ops.__call ) )( i + 5 );
     }
 }
