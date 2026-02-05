5859e5eb49c56ee9aa64ad4878293ed4b7d33f14
huanglingang <lingang_huang@vastaitech.com>
2026-02-05 13:58:40 +0800

    [mod] add watch-dog

---
+++ watch-dog/invoker.h
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <memory>
+#include <mutex>
+#include <condition_variable>
+#include <sstream>
+#include <string>
+#include <unistd.h>
+#include <tuple>
+#include <utility>
+
+using namespace std;
+
+class WatchDog
+{
+public:
+    condition_variable         _M_cond;
+    std::mutex                 _M_mutex;
+    unsigned long              _M_touch_ts;
+    unsigned long              _M_sched_count;
+    unsigned long              _M_last_count;
+    unsigned long              _M_last_sched_ts;
+    static bool                running;
+    static const unsigned long TIMEOUT = 5;  // seconds
+    WatchDog() : _M_mutex(), _M_touch_ts( 0 ), _M_sched_count( 0 ), _M_last_count( 0 )
+    {
+        running = true;
+        thread t1( &WatchDog::work, this );
+        thread t2( &WatchDog::touch, this );
+        thread t3( &WatchDog::watch, this );
+        t1.join();
+        t2.join();
+        t3.join();
+    }
+    void work()
+    {
+        while ( running )
+        {
+            printf( "do something...\n" );
+            this_thread::sleep_for( chrono::seconds( 2 ) );
+            ++_M_sched_count;
+        }
+    }
+    void touch()
+    {
+        static int count = 0;
+        while ( running )
+        {
+            std::unique_lock< std::mutex > lock( _M_mutex );
+            _M_touch_ts = static_cast< unsigned long >( time( nullptr ) );
+            printf( "touch dog at %lu\n", _M_touch_ts );
+            do
+            {
+                _M_cond.wait( lock );
+            } while ( ++count > 15 && running );
+        }
+    }
+    void watch()
+    {
+        while ( running )
+        {
+            this_thread::sleep_for( chrono::seconds( 1 ) );
+            do
+            {
+                std::unique_lock< std::mutex > lock( _M_mutex );
+                _M_cond.notify_one();
+            } while ( 0 );
+            if ( !_M_touch_ts )
+            {
+                _M_touch_ts = static_cast< unsigned long >( time( nullptr ) );
+                continue;
+            }
+            auto now = static_cast< unsigned long >( time( nullptr ) );
+            // 通过判断看门狗的时间戳判断看门狗线程[进程]在一段时间内是否得到调度 //
+            if ( now > _M_touch_ts + TIMEOUT )
+            {
+                std::cout << "watch dog timeout, last sched ts: " << _M_touch_ts << ", now: " << now << std::endl;
+                running = false;
+                _M_cond.notify_all();
+                break;
+            }
+            check_hung_task( now );
+        }
+    }
+    // 检查正常 [task] 是否长时间得不到调度 //
+    void check_hung_task( unsigned long __now )
+    {
+        if ( _M_last_count != _M_sched_count )
+        {
+            _M_last_count    = _M_sched_count;
+            _M_last_sched_ts = __now;
+            return;
+        }
+        printf( "check hung task now: %lu, _M_last_sched_ts: %lu\n", __now, _M_last_sched_ts );
+        if ( __now > _M_last_sched_ts + TIMEOUT )
+        {
+            std::cout << "detected hung task, last sched count: " << _M_last_count << ", now count: " << _M_sched_count
+                      << std::endl;
+            running = false;
+            _M_cond.notify_all();
+        }
+    }
+};
+bool WatchDog::running = false;
---
+++ watch-dog/test.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <iostream>
+#include <algorithm>
+#include <memory>
+#include <tuple>
+#include <sstream>
+#include <thread>
+#include "invoker.h"
+
+using namespace std;
+
+int main()
+{
+
+    WatchDog wd;
+    return 0;
+}
