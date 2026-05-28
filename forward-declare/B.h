e8a406123bb4af579dcf447e087b1283ca931ece
huanglingang <lingang_huang@vastaitech.com>
2026-05-28 14:40:55 +0800

    [mod] code-check for forward-declare

--- forward-declare/B.h
+++ forward-declare/B.h
@@ -1,6 +1,6 @@
 #ifndef __B_H__
 #define __B_H__
-/*DO not want to include B.h, use forward declare instead */
+/*DO not want to include A.h, use forward declare instead */
 /*避免头文件循环引用的问题 */
 class A;  // 前置声明
 class B
